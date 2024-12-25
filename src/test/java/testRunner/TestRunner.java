05a137171fb73064c6f5d3a823cc90c40cb553de
SDET185QA@gmail.com <SDET185QA@gmail.com>
2024-12-25 11:06:17 -0500

    added changes in tree POF to overcome the maven test error

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 	plugin = {"pretty", "html:target/dsalgoBDD.html"}, //reporting purpose
 		monochrome=false,  //console output color
 		tags = "",//tags from feature file
-		features = {"src/test/resources/features"}, //location of feature files
+		features = {"src/test/resources/features/F05_Array.feature"}, //location of feature files
 		glue= {"stepDefinitions","hooks"}) //location of step definition files
 public class TestRunner extends AbstractTestNGCucumberTests {
 	
