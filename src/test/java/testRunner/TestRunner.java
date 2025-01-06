d183a056115650a5ec6788f97afd45f4b23617bd
SDET185QA@gmail.com <SDET185QA@gmail.com>
2025-01-05 19:59:01 -0500

    extend report and cross browser testing

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	plugin = {"pretty", "html:target/dsalgoBDD.html"}, //reporting purpose
+	plugin = {"pretty", "html:target/dsalgoBDD.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
 		monochrome=false,  //console output color
 		tags = "",//tags from feature file
 		features = {"src/test/resources/features"}, //location of feature files
