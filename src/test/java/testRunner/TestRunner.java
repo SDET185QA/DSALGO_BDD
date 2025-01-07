f293c04ffa573a6b1299564e3bd848158c158488
SDET185QA@gmail.com <SDET185QA@gmail.com>
2025-01-06 23:45:39 -0500

    cucmber report chnages

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -5,11 +5,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	plugin = {"pretty", "html:target/dsalgoBDD.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
-		monochrome=false,  //console output color
-		tags = "",//tags from feature file
-		features = {"src/test/resources/features"}, //location of feature files
-		glue= {"stepDefinitions","hooks"}) //location of step definition files
+	plugin = {"pretty", "html:target/dsalgoBDD.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","json:target/cucumber-reports/Cucumber.json","html:target/cucumber-reports/dsalgo.html"}, //reporting purpose
+			publish = true,	
+			monochrome=false,  //console output color
+			tags = "",//tags from feature file
+			features = {"src/test/resources/features"}, //location of feature files
+			glue= {"stepDefinitions","hooks"}) //location of step definition files
 public class TestRunner extends AbstractTestNGCucumberTests {
 	
 	@Override
