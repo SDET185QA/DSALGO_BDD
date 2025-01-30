1cca150d480d4ca10f8b5cc8f14b4a4cb2388682
SDET185QA@gmail.com <SDET185QA@gmail.com>
2025-01-30 14:19:13 -0500

    testrunner to check jira zephr

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -3,13 +3,15 @@ package testRunner;
 import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 @CucumberOptions(
-	plugin = {"pretty", "html:target/dsalgoBDD.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","json:target/cucumber-reports/Cucumber.json","html:target/cucumber-reports/dsalgo.html"}, //reporting purpose
+	plugin = {"pretty", "html:target/dsalgoBDD.html","json:target/cucumber-reports/Cucumber.json",
+			"html:target/cucumber-reports/dsalgo.html",
+			"com.aventstack.chaintest.plugins.ChainTestCucumberListener:" }, //reporting purpose
 			publish = true,	
 			monochrome=false,  //console output color
 			tags = "",//tags from feature file
-			features = {"src/test/resources/features"}, //location of feature files
+			features = {"src/test/resources/features/LoginTest.java"}, //location of feature files
 			glue= {"stepDefinitions","hooks"}) //location of step definition files
 public class TestRunner extends AbstractTestNGCucumberTests {
 	
